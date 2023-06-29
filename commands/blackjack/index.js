c7a6c283c45ae96ea03966a7a42779dd18ed9926
othsueh <ych930719@gmail.com>
2023-06-29 10:41:59 +0800

    Created 52 cards

--- commands/blackjack/index.js
+++ commands/blackjack/index.js
@@ -1,111 +1,120 @@
-const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, Client} = require('discord.js');
-const fs = require('node:fs');
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder,
+    Client,
+} = require("discord.js");
+const fs = require("node:fs");
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('blackjack').setDescription('Play blackjack! (21 點)'),
-	/**
- 	*
- 	* @param {Client} client
- 	* @param {import('discord.js').Interaction} interaction
- 	*/
-	async execute(client, interaction) {
+    data: new SlashCommandBuilder().setName("blackjack").setDescription("Play blackjack! (21 點)"),
+    /**
+     *
+     * @param {Client} client
+     * @param {import('discord.js').Interaction} interaction
+     */
+    async execute(client, interaction) {
+        //topic : 52張牌
+        const suits = ["♠", "♥", "♦", "♣"];
+        let cards = new Array();
+        suits.forEach((suits) => {
+            for (let i = 1; i <= 13; i++) {
+                cards.push(suits + i);
+            }
+        });
 
-    	//建立 embed 和剪刀石頭布的三個 button
-    	const buttonEmbed = new EmbedBuilder()
-        	.setColor('#5865F2')
-        	.setTitle(`來猜拳！`);
+        // // Old dice Program
+        //建立 embed 和剪刀石頭布的三個 button
+        const buttonEmbed = new EmbedBuilder().setColor("#5865F2").setTitle(`來猜拳！`);
 
-    	const hitButton = new ButtonBuilder()
-        	.setCustomId('hit')
-        	.setLabel('🃏')
-        	.setStyle(ButtonStyle.Primary);
-        
+        const hitButton = new ButtonBuilder()
+            .setCustomId("hit")
+            .setLabel("🃏")
+            .setStyle(ButtonStyle.Primary);
 
         const stopButton = new ButtonBuilder()
-        .setCustomId('stop')
-        .setLabel('🚫')
-        .setStyle(ButtonStyle.Primary);
+            .setCustomId("stop")
+            .setLabel("🚫")
+            .setStyle(ButtonStyle.Primary);
 
+        //將三個 button 都放入 row 中並回覆 embed 和 row
+        //你要做的事很像 row = build row ( components = buttonScissors , buttonRock , buttonPaper )
+        //TODO 2: buttonRow 等於什麼呢？
+        //const buttonRow = ...
+        const buttonRow = new ActionRowBuilder().addComponents(
+            scissorButton,
+            paperButton,
+            stoneButton,
+        );
 
-    	//將三個 button 都放入 row 中並回覆 embed 和 row
-    	//你要做的事很像 row = build row ( components = buttonScissors , buttonRock , buttonPaper )
-    	//TODO 2: buttonRow 等於什麼呢？
-    	//const buttonRow = ...
-        const buttonRow = new ActionRowBuilder()
-        .addComponents(scissorButton,paperButton,stoneButton);
+        interaction.reply({ embeds: [buttonEmbed], components: [buttonRow] });
 
-    	interaction.reply({ embeds: [buttonEmbed], components: [buttonRow] });
+        //建立 collector
+        const collector = interaction.channel.createMessageComponentCollector({ time: 15000 });
 
-    	//建立 collector
-    	const collector = interaction.channel.createMessageComponentCollector({ time: 15000 });
+        //等待 collector 蒐集到玩家案的按鈕
+        collector.on("collect", async (collected) => {
+            //電腦隨機出拳
+            //0 is scissors, 1 is rock, 2 is paper
+            const computerChoice = Math.floor(Math.random() * 3);
 
-    	//等待 collector 蒐集到玩家案的按鈕
-    	collector.on('collect', async collected => {
+            //利用玩家所按按鈕的 customId 來判斷玩家的選擇
+            let playerChoice;
+            //TODO 3: Do the same thing in rock and paper
 
-        	//電腦隨機出拳
-        	//0 is scissors, 1 is rock, 2 is paper
-        	const computerChoice = Math.floor(Math.random() * 3);
+            if (collected.customId === "scissors") {
+                playerChoice = 0;
+            } else if (collected.customId === "stone") {
+                playerChoice = 1;
+            } else if (collected.customId === "paper") {
+                playerChoice = 2;
+            }
 
-        	//利用玩家所按按鈕的 customId 來判斷玩家的選擇
-        	let playerChoice;
-        	//TODO 3: Do the same thing in rock and paper
-        	
-        	if (collected.customId === 'scissors') {
-            	playerChoice = 0;
-        	}
-            else if (collected.customId === 'stone') {
-            	playerChoice = 1;
-        	}
-            else if (collected.customId === 'paper') {
-            	playerChoice = 2;
-        	}
-        
-
-
-
-        	//判斷玩家勝利，電腦勝利或平手
-        	let winner;
-            switch(playerChoice){
+            //判斷玩家勝利，電腦勝利或平手
+            let winner;
+            switch (playerChoice) {
                 case 0:
-                    switch(computerChoice){
+                    switch (computerChoice) {
                         case 0:
                             winner = 0;
                             break;
                         case 1:
-                            winner=-1;
+                            winner = -1;
                             break;
                         case 2:
-                            winner=1
+                            winner = 1;
                             break;
                         default:
                             break;
                     }
                     break;
-                case 1: 
-                    switch(computerChoice){
+                case 1:
+                    switch (computerChoice) {
                         case 0:
-                            winner=1;
+                            winner = 1;
                             break;
                         case 1:
-                            winner=0;
+                            winner = 0;
                             break;
                         case 2:
-                            winner=-1;
+                            winner = -1;
                             break;
                         default:
                             break;
                     }
                     break;
                 case 2:
-                    switch(computerChoice){
+                    switch (computerChoice) {
                         case 0:
-                            winner=-1;
+                            winner = -1;
                             break;
                         case 1:
-                            winner=1;
+                            winner = 1;
                             break;
                         case 2:
-                            winner=0;
+                            winner = 0;
                             break;
                         default:
                             break;
@@ -113,61 +122,58 @@ module.exports = {
                     break;
             }
 
-
-        	//從結果計算獲得/失去的 money
-        	let earnings;
-            switch(winner){
+            //從結果計算獲得/失去的 money
+            let earnings;
+            switch (winner) {
                 case 0:
-                    earnings=0;
+                    earnings = 0;
                     break;
                 case 1:
-                    earnings=10;
+                    earnings = 10;
                     break;
                 case -1:
-                    earnings=-10;
+                    earnings = -10;
                     break;
                 default:
                     break;
             }
 
+            //讀取 player.json 並 parse 成 players
+            const data = fs.readFileSync("player.json");
+            const players = JSON.parse(data);
 
-        	//讀取 player.json 並 parse 成 players
-        	const data = fs.readFileSync('player.json');
-        	const players = JSON.parse(data);
+            //在所有資料中尋找呼叫此指令玩家的資料
+            let found = false;
+            for (let j = 0; j < players.length; j++) {
+                //如果有就修改該玩家的 money 並回覆結果
+                if (players[j].id == interaction.user.id) {
+                    found = true;
+                    players[j].money += earnings;
+                    const resultEmbed = new EmbedBuilder()
+                        .setColor("#5865F2")
+                        .setTitle("剪刀石頭布！")
+                        .setDescription(`結果：${earnings}元\n你現在有 ${players[j].money} 元!`);
+                    collected.update({ embeds: [resultEmbed], components: [] });
+                    break;
+                }
+            }
 
-        	//在所有資料中尋找呼叫此指令玩家的資料
-        	let found = false;
-        	for (let j = 0; j < players.length; j++) {
+            //如果沒有資料就創建一個新的並回覆結果
+            if (found == false) {
+                players.push({ id: interaction.user.id, money: 500 });
+                const resultEmbed = new EmbedBuilder()
+                    .setColor("#5865F2")
+                    .setTitle("剪刀石頭布！")
+                    .setDescription(`結果：${earnings}元\n你現在有 ${500 + earnings} 元!`);
+                collected.update({ embeds: [resultEmbed], components: [] });
+            }
 
-            	//如果有就修改該玩家的 money 並回覆結果
-            	if (players[j].id == interaction.user.id) {
-                	found = true;
-                	players[j].money += earnings;
-                	const resultEmbed = new EmbedBuilder()
-                    	.setColor('#5865F2')
-                    	.setTitle('剪刀石頭布！')
-                    	.setDescription(`結果：${earnings}元\n你現在有 ${players[j].money} 元!`);
-                	collected.update({ embeds: [resultEmbed], components: [] });
-                	break;
-            	}
-        	}
+            //stringify players 並存回 player.json
+            const json = JSON.stringify(players);
+            fs.writeFileSync("player.json", json);
 
-        	//如果沒有資料就創建一個新的並回覆結果
-        	if (found == false) {
-            	players.push({ id: interaction.user.id, money: 500 });
-            	const resultEmbed = new EmbedBuilder()
-                	.setColor('#5865F2')
-                	.setTitle('剪刀石頭布！')
-                	.setDescription(`結果：${earnings}元\n你現在有 ${500 + earnings} 元!`);
-            	collected.update({ embeds: [resultEmbed], components: [] });
-        	}
-
-        	//stringify players 並存回 player.json
-        	const json = JSON.stringify(players);
-        	fs.writeFileSync('player.json', json);
-
-        	//關閉 collector
-        	collector.stop();
-    	});
-	}
+            //關閉 collector
+            collector.stop();
+        });
+    },
 };
--- commands/embed.js
+++ commands/embed.js
@@ -1,22 +1,14 @@
-const {SlashCommandBuilder,EmbedBuilder}=require('discord.js')
-module.exports={
-    data: new SlashCommandBuilder().setName('embed').setDescription('embed'),
-    async execute(client,interaction){
-        const embed = new EmbedBuilder().setTitle('I\'m an Embed')
-        .setColor('Red')
-        .setDescription('Hello world!')
-        .addFields([
-            {
-                name: 'field 1',
-                value: 'field 1 value',
-                inline: true
-            },
-            {
-                name: 'field 2',
-                value: 'field 2 value',
-                inline: true
-            }
-        ])
-        interaction.reply({embeds:[embed]})
-    }
-}
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder().setName("embed").setDescription("embed"),
+    async execute(client, interaction) {
+        const embed = new EmbedBuilder()
+            .setTitle("I'm an Embed")
+            .setColor("#ffffff")
+            .setDescription("Hello world!")
+            .setImage(
+                "https://media0.giphy.com/media/N9SJ8eZZaHZQGDsneP/giphy.gif?cid=ecf05e47er7yxmcsd138me6e9ck9bkwf1je2wss6f20iawhw&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+            );
+        interaction.reply({ embeds: [embed] });
+    },
+};
